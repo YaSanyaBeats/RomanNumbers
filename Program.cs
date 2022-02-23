33170bf44b25a0ff07be9c443b6e8208684b904c
YaSanyaBeats <75445623+YaSanyaBeats@users.noreply.github.com>
2022-02-23 21:36:16 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        RomanNumber a = new RomanNumber(2459);
+        RomanNumber b = new RomanNumber(2999);
+        RomanNumber c = new RomanNumber(4);
+        RomanNumber d = new RomanNumber(500);
+
+        Console.WriteLine("A = 2459 = MMCDLIX: " + a.ToString());
+        Console.WriteLine("B = 2999 = MMCMXCIX: " + b.ToString());
+        Console.WriteLine("C = 4 = IV: " + c.ToString());
+        Console.WriteLine("D = 500 = D : " + d.ToString());
+        Console.WriteLine("");
+
+        Console.WriteLine("B + C = 3003 = MMMIII: " + RomanNumber.Add(b, c).ToString());
+        Console.WriteLine("B - C = 2995 = MMCMXCV: " + RomanNumber.Sub(b, c).ToString());
+        Console.WriteLine("D * C = 2000 = MM: " + RomanNumber.Mul(d, c).ToString());
+        Console.WriteLine("D / C = 125 = CXXV: " + RomanNumber.Div(d, c).ToString());
+        Console.WriteLine("");
+
+        Console.WriteLine("Сортировка");
+        RomanNumber[] numbers = { a, b, c, d };
+        Array.Sort(numbers);
+        foreach (RomanNumber number in numbers)
+        {
+            Console.WriteLine(number.ToString());
+        }
+        Console.WriteLine("");
+
+        Console.WriteLine("Копирование");
+        var f = (RomanNumber)c.Clone();
+        Console.WriteLine(f.ToString());
+    }
+}
---
+++ RomanNumber.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class RomanNumber : ICloneable, IComparable
+{
+    private string romanNumber = "";
+    private ushort decimalNumber;
+    //Конструктор получает число n, которое должен представлять объект класса
+    public RomanNumber(ushort n)
+    {
+        decimalNumber = n;
+
+    }
+    //Сложение римских чисел
+    public static RomanNumber Add(RomanNumber? n1, RomanNumber? n2)
+    {
+        if(n1 == null || n2 == null)
+        {
+            throw new RomanNumberException("Некорректные входные данные");
+        }
+        return new RomanNumber ((ushort)(n1.decimalNumber + n2.decimalNumber));
+    }
+    //Вычитание римских чисел
+    public static RomanNumber Sub(RomanNumber? n1, RomanNumber? n2)
+    {
+        if (n1 == null || n2 == null || n1.decimalNumber - n2.decimalNumber <= 0)
+        {
+            throw new RomanNumberException("Некорректные входные данные");
+        }
+
+        return new RomanNumber((ushort)(n1.decimalNumber - n2.decimalNumber));
+    }
+    //Умножение римских чисел
+    public static RomanNumber Mul(RomanNumber? n1, RomanNumber? n2)
+    {
+        if (n1 == null || n2 == null)
+        {
+            throw new RomanNumberException("Некорректные входные данные");
+        }
+        return new RomanNumber((ushort)(n1.decimalNumber * n2.decimalNumber));
+    }
+    //Целочисленное деление римских чисел
+    public static RomanNumber Div(RomanNumber? n1, RomanNumber? n2)
+    {
+        if (n1 == null || n2 == null || n1.decimalNumber / n2.decimalNumber <= 0)
+        {
+            throw new RomanNumberException("Некорректные входные данные");
+        }
+
+        return new RomanNumber((ushort)(n1.decimalNumber / n2.decimalNumber));
+    }
+    //Возвращает строковое представление римского числа
+    public override string ToString()
+    {
+        if(romanNumber == "")
+        {
+            ushort n = decimalNumber;
+            if (n <= 0 || n >= 4000)
+            {
+                throw new RomanNumberException("Некорректное десятичное число");
+            }
+
+            string[] romanSymbols = new string[] { "M", "D", "C", "L", "X", "V", "I" };
+
+            //численное текущего значение римского символа
+            ushort romanSymbolValue = 1000;
+
+            for (int i = 0; i < romanSymbols.Length; i += 2)
+            {
+                //берём старшую цифру числа
+                int digit = n / romanSymbolValue;
+
+                if (digit == 9)
+                {
+                    romanNumber += romanSymbols[i] + romanSymbols[i - 2];
+                    digit = 0;
+                }
+                if (digit >= 5)
+                {
+                    romanNumber += romanSymbols[i - 1];
+                    digit -= 5;
+                }
+                if (digit == 4)
+                {
+                    romanNumber += romanSymbols[i] + romanSymbols[i - 1];
+                    digit = 0;
+                }
+                for (int j = 0; j < digit; j++)
+                {
+                    romanNumber += romanSymbols[i];
+                }
+
+                //обрубаем старшую цифру числа
+                n %= romanSymbolValue;
+
+                //уменьшаем численное значение символа в 10 раз
+                romanSymbolValue /= 10;
+            }
+        }
+        return romanNumber;
+    }
+
+    public object Clone()
+    {
+        return new RomanNumber(decimalNumber);
+    }
+
+    public int CompareTo(object? o)
+    {
+        if (o is RomanNumber number)
+        {
+            return number.decimalNumber - decimalNumber;
+        }
+        else throw new ArgumentException("Некорректное значение параметра");
+    }
+}
